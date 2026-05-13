0b48af440f3cb7be87a2eabf0924adcc21c19169
Francisco Sales <salesf345@gmail.com>
2026-05-13 14:35:57 -0300

    Create a tree file where separates the raw json and the treated one, also improve the dockerfile, fixing automations problems

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM node:20
+FROM python:3.12-slim
 
 WORKDIR /workspace
 
-RUN apt-get update && apt-get install -y git curl python3 python3-pip
+RUN apt-get update && apt-get install -y git curl 
 
 RUN pip3 install --break-system-packages semgrep
 
@@ -10,5 +10,5 @@ COPY scripts/ /scripts/
 
 RUN chmod +x /scripts/*.sh
 
-ENTRYPOINT ["/bin/bash"]
+ENTRYPOINT ["/scripts/run_semgrep.sh"]
 
--- scripts/run_semgrep.sh
+++ scripts/run_semgrep.sh
@@ -11,11 +11,19 @@ fi
 REPO_URL=$1
 
 echo "Clonando repositório..."
-git clone "${REPO_URL}" repo
+REPO_NAME=$(basename $REPO_URL)
+git clone "${REPO_URL}" $REPO_NAME
 
-cd repo
+mkdir -p /workspace/results
+
+
+cd $REPO_NAME
 
 echo "Rodando Semgrep..."
-semgrep scan --config=auto --json --output /workspace/result.json
+semgrep scan --config=auto --json --output /workspace/results/$REPO_NAME.json
+
+mkdir -p /workspace/treated_results
+
+cat /workspace/results/$REPO_NAME.json | python3 -m json.tool>/workspace/treated_results/treated_$REPO_NAME.json 
 
 echo "Análise finalizada!"
